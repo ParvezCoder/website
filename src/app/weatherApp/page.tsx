42d8fbcfd426fa26fe05a58a42e0fafdf42b6002
Parvez Coder <ParvezCoder786@gmail.com>
2024-10-14 02:06:38 +0500

    weather app

--- src/app/weatherApp/page.tsx
+++ src/app/weatherApp/page.tsx
@@ -3,10 +3,18 @@ import React, { useState } from 'react'
 import { FaSearch } from "react-icons/fa";
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import Clear from '../../../public/weatherImages/Clear.png'
+import Error from '../../../public/weatherImages/error.png'
+import Smoke from '../../../public/weatherImages/Smoke.png'
+import Rain from '../../../public/weatherImages/Rain.png'
+import Cloud from '@/../public/weatherImages/Cloud.png'
+import { DiVim } from 'react-icons/di';
 
 function WeatherApp() {
+
     const [search, setSearch] = useState("")
-    const [dataFromApi, setdataFromApi] = useState<any>({})
+    const [data, setdata] = useState<any>({})
+    const [error, setError] = useState("")
     const api = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
     const apiKeys = "17db3021b549f3ab58682d0dd9df299d"
     const getData = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,36 +22,77 @@ function WeatherApp() {
     }
 
     const fun = async () => {
-        if (search == "") {
-            alert("enter city name")
-        }
-        const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=17db3021b549f3ab58682d0dd9df299d`)
+        const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=17db3021b549f3ab58682d0dd9df299d&units=metric`)
         const jsonData = await apiData.json()
-        setdataFromApi(jsonData)
-        console.log(dataFromApi)
+        setdata(jsonData)
+        if (search == "") {
+            setError("Please Enter City Name")
+        }
+        else if (jsonData.cod === "404") {
+            setError("Please Enter  valid city name")
+        }
+        else {
+            setError("")
+        }
+
+        setSearch("")
+
+
     }
-    
+    console.log(data)
+
+
     return (
         <>
             <Navbar />
-            <div className='bg-gray-800 h-screen flex justify-center items-center'>
-                <div className='text-black bg-white  p-4 rounded-lg'>
+            <div className='bg-gray-800 h-screen flex justify-center '>
+                <div className=' bg-red-800  h-2/3 p-4 mt-10 text-white font-bold text-lg rounded-lg'>
                     <h1 className='font-bold text-2xl text-center'>Weather Widget</h1>
                     <p className='text-xl'>Search for the current weather condition in your city</p>
                     <div className='flex justify-center items-center mt-4'>
                         <input
                             type='text'
-                            className='border  !border-gray-500  pl-2 rounded '
-                            placeholder='search city name'
+                            className='border  p-2 bg-black !border-gray-500  pl-2 rounded '
+                            placeholder='Enter city name'
                             onChange={getData}
+                            value={search}
+                            
+
                         />
                         <button onClick={fun} className='ml-3'>
-                            <div className=' p-0.5'>
-                                <FaSearch className='  font-semibold text-xl' />
+                            <div className=' p-1.5'>
+                                <FaSearch className=' - font-semibold text-2xl' />
                             </div>
                         </button>
                     </div>
-                    <p className='text-center mt-4'>The Tempreture of {dataFromApi.name}  is 37C</p>
+                    <div>
+
+                        {data && data.weather ?
+
+                            <div className='flex flex-col items-center'>
+                                <h1 className='text-center mt-4'>The Temperature of {data.name}</h1>
+                                <div className='mt-4 h-48 w-48'>
+                                    <img src={data.weather[0].main === "Clear" ? Clear.src : ""} />
+                                    <img src={data.weather[0].main === "Smoke" ? Smoke.src : ""} />
+                                    <img src={data.weather[0].main === "Haze" ? Cloud.src : ""} />
+                                    <img src={data.weather[0].main === "Clouds" ? Cloud.src : ""} />
+                                    <img src={data.weather[0].main === "Rain" ? Rain.src : ""} />
+                                </div>
+                                <p className='text-center mt-4'>{Math.floor(data.main.temp)}°C</p>
+                                <p className='text-center mt-4'>{data.weather[0].description}</p>
+                            </div>
+                            : ""
+                        }
+                    </div>
+                    <div>
+                        {error ? <div className='flex flex-col items-center'>
+                            <div className='mt-4 h-48 w-48'>
+                                <img src={Error.src} alt="Error"  className='rounded-xl'/>
+                            </div>
+                            <h1 className='text-center mt-4 border-blue-700  border-2 p-1.5 shadow-xl rounded-md'>{error}</h1>
+                            
+                        </div> : ""}
+                    </div>
                 </div>
             </div>
             <Footer />
--- src/portfolio/NavbarWebsite.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Navbar = () => {
-  return (
-    <div>Website using  Next Js</div>
-  )
-}
-
-export default Navbar
