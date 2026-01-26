8d4c9d0dfd464f32f9604a484e2af57254730c0f
parvez <parvezcoder786@gmail.com>
2026-01-26 20:30:55 +0500

    coderr6

--- src/app/Components/ChatBot.tsx
+++ src/app/Components/ChatBot.tsx
@@ -8,7 +8,7 @@ const ChatBot: React.FC = () => {
   useEffect(() => {
     createChat({
       webhookUrl:
-        "https://coderr5.app.n8n.cloud/webhook/991c4c90-379d-43e8-af0c-7a06f8397052/chat",
+        "https://coderr6.app.n8n.cloud/webhook/991c4c90-379d-43e8-af0c-7a06f8397052/chat",
         webhookConfig: {
         method: "POST",
         headers: {},
